0f6a218efc43767818e2502c4129eac07c645bf8
mzulfikaras <zulfikaralisunan96@gmail.com>
2020-12-14 18:03:29 +0700

    Personal Board

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="zulfikar">
+
+    <title>Resume - Zulfikar Alisunan</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@master/devicon.min.css">
+    <link href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/devicons/css/devicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/resume.min.css')}}" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none"></span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{asset('img/zulfikar1.JPG')}}" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Muhammad Zulfikar
+            <span class="text-primary">Alisunan</span>
+          </h1>
+          <div class="subheading mb-5">Green Savana, Citra Raya · Kab.Tangerang, Banten · (+62) 899-4407-084 ·
+            <a href="mailto:zulfikaralisunan96@gmail.com">zulfikaralisunan96@gmail.com</a>
+          </div>
+          <p class="mb-5">
+            I am a Web Developer. Living in Tangerang, Indonesia. Experience using Laravel, JavaScript, PHP, HTML, CSS and more. Able to work with a team, and always have an open mind.
+            Enjoy communicating, researching new things, and solving problems with the best solutions.</p>
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/zulfikar.alisunan/">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/muhammad-zulfikar-alisunan-40620a17a/">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/mzulfikaras">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Remote Web Developer</h3>
+              <div class="subheading mb-3">PT. Astra Digital Arta</div>
+              <p>
+                Create employee administrative data in the company. configuration with local server. Work remotely.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary"> September 2020 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Instructor Fullstack Web Developer</h3>
+              <div class="subheading mb-3">Kampoos.id</div>
+              <p>Train prospective full stack web developers, from the beginning to the end they can become a reliable web developer. Creating material about web developers, for students to learn.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2020 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Fullstack Web Developer</h3>
+              <div class="subheading mb-3">IMPORTIR.ORG</div>
+              <p>
+                Creating a digital-based corporate system. Take care of the running system. And fix the error in the system. Creating an attractive frontend of the website for the user.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">March 2020 - Present</span>
+            </div>
+          </div>
+
+          {{-- <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Web Design Intern</h3>
+              <div class="subheading mb-3">Shout! Media Productions</div>
+              <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">September 2008 - June 2010</span>
+            </div>
+          </div> --}}
+
+        </div>
+
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Edutech.id</h3>
+              <div class="subheading mb-3">Web Developer Certification</div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">November 2019 - March 2020</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Multimedia Nusantara University</h3>
+              <div class="subheading mb-3">Bachelor of Computer Science</div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2014 - June 2019</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Citra Islami High School</h3>
+              <div class="subheading mb-3">Science</div>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2011 - June 2014</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicon-html5-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-css3-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-bootstrap-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-javascript-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-jquery-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-vuejs-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-mysql-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-php-plain"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-laravel-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-bitbucket-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-wordpress-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-visualstudio-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-github-plain-wordmark"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicon-npm-original-wordmark"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">Workflow</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Responsive Design, The Function Runs Smoothly</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Cross Browser Testing &amp; Debugging</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Cross Functional Teams</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Development &amp; Scrum</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>As a web developer, I like to learn new knowledge, the latest knowledge that is currently being widely used and which is currently in high demand. happy with lonely circumstances, and prefer to be alone.</p>
+          <p class="mb-0">
+            During free time, it's nice to sleep and drink a cup of coffee. And prefer to make personal projects to add to the portfolio.</p>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="contact">
+        <div class="my-auto">
+          <h2 class="mb-5">Contact Me</h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <form>
+                  <div class="form-group">
+                    <input type="name" class="form-control" id="exampleInputName" placeholder="Your Full Name...">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your Email Address...">
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" aria-label="With textarea" style="height: 17rem"></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-warning btn-lg btn-block">Submit</button>
+                </form>
+              </div>
+              <div  class="col-md-6">
+                <h5>Address: <small class="text-muted">Green Savana, Citra Raya Blok N14/02, Tangerang, Banten, Indonesia</small></h5>
+                <h5>Email: <small class="text-muted">zulfikaralisunan96@gmail.com</small></h5>
+                <h5>Contact: <small class="text-muted">+62 899 4407 084</small></h5>
+                <!-- <p><iframe width="600" height="450" frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJtcNatvRsrjsRA38LEBPt_78&key=..." allowfullscreen></iframe></p> -->
+                <div class="text-center">
+                  <img class="img-fluid contact-image" alt="Responsive image" src="https://csds.qld.edu.au/sdc/resources/images/find-us-map.jpg" class="rounded" alt="...">
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{asset('js/resume.min.js')}}"></script>
+
+  </body>
+
+</html>
